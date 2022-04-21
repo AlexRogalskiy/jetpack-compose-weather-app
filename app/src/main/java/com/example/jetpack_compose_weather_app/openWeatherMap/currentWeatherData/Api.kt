3f0d26b8bea13ca6a2d8ee769bcd8b834f4f711e
JonasJore <jonas.jore@gmail.com>
2022-04-21 19:32:04 +0200

    oops

--- app/src/main/java/com/example/jetpack_compose_weather_app/openWeatherMap/currentWeatherData/Api.kt
+++ app/src/main/java/com/example/jetpack_compose_weather_app/openWeatherMap/currentWeatherData/Api.kt
@@ -1,10 +1,11 @@
 package com.example.jetpack_compose_weather_app.openWeatherMap.currentWeatherData
 
+import com.example.jetpack_compose_weather_app.openWeatherMap.keys
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Api {
-    @GET("weather?appid=612484d7c358a741d0646892ee97219b") // TODO: store api-key somewhere safe...
+    @GET("weather?appid=${keys.OPEN_WEATHER_API_KEY}")
     suspend fun getByLocation(
         @Query("q") location: String
     ): CurrentWeather
